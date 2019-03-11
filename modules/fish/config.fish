9bc9d37525f9d2af1d5c7858b9740376f39059a0
takeokunn <bararararatty@gmail.com>
2019-03-11 18:18:16 +0900

    Update

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -11,27 +11,6 @@ function fish_prompt
     printf '%s %s' (set_color yellow)(prompt_pwd) $status_face
 end
 
-# for composer
-set PATH $PATH $HOME/.composer/vendor/bin
-
-# for gem
-set -x GEM_HOME $HOME/.gem
-set -x PATH $PATH $GEM_HOME/bin
-
-# for golang
-set -x GOPATH $HOME/projects/go
-set -x PATH $PATH $GOPATH/bin
-
-# for gcloud
-set -x PATH $PATH $HOME/google-cloud-sdk/bin
-
-# colorrc
-set -x PATH $PATH (brew --prefix coreutils)/libexec/gnubin
-bass (dircolors ~/.colorrc)
-
-# roswell
-set -x PATH $PATH $HOME/.roswell/bin
-
 function logo
     echo '                 '(set_color F00)'___
   ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
@@ -52,9 +31,6 @@ end
 
 # alias
 alias cdd 'cd ~/Desktop'
-alias e 'emacs'
-alias ss 'open /System/Library/CoreServices/ScreenSaverEngine.app'
-alias ls 'gls --color'
 
 # peco
 function fish_user_key_bindings
