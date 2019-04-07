37f830596032b950d298590ff9675f0cbf5f3260
takeokunn <bararararatty@gmail.com>
2019-04-07 18:31:52 +0900

    Update xkeysnail

--- modules/xkeysnail/dot.config.py
+++ modules/xkeysnail/dot.config.py
@@ -3,35 +3,32 @@
 import re
 from xkeysnail.transform import *
 
-define_modmap({
-   Key.CAPSLOCK: Key.LEFT_CTRL
+define_conditional_modmap(re.compile(r'Firefox'), {
+    Key.CAPSLOCK: Key.LEFT_CTRL,
 })
 
-define_multipurpose_modmap(
-   { Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL] }
-)
-
 define_keymap(re.compile("Firefox"), {
-   # change tab
-   K("C-f"): K("C-Tab"),
-   K("C-b"): K("C-Shift-Tab"),
+    # change tab
+    K("C-f"): K("C-Tab"),
+    K("C-b"): K("C-Shift-Tab"),
 
-   # search
-   K("C-s"): K("C-f"),
-   K("C-s"): K("C-g"),
-   K("C-r"): K("C-Shift-g"),
+    # search
+    K("C-s"): K("C-f"),
+    K("C-s"): K("C-g"),
+    K("C-r"): K("C-Shift-g"),
 
-   # private window
-   K("C-Shift-n"): K("C-Shift-p"),
+    # private window
+    K("C-Shift-n"): K("C-Shift-p"),
 
-   # up/down
-   K("C-n"): K("DOWN"),
-   K("C-p"): K("UP"),
-   K("C-v"): K("SPACE"),
+    # up/down
+    K("C-n"): K("DOWN"),
+    K("C-p"): K("UP"),
+    K("C-v"): K("SPACE"),
+    K("C-v"): K("SPACE"),
 
-   K("C-g"): {
-       # change page
-       K("C-b"): K("C-LEFT_BRACE"),
-       K("C-f"): K("C-RIGHT_BRACE"),
-   }
+    K("C-g"): {
+        # change page
+        K("C-b"): K("C-LEFT_BRACE"),
+        K("C-f"): K("C-RIGHT_BRACE"),
+    }
 }, "Firefox")
