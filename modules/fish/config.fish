6587184ddbf986e190ccdfea85a10bd554de2cfe
takeokunn <bararararatty@gmail.com>
2019-05-07 16:17:47 +0900

    Add z

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -32,9 +32,25 @@ end
 # alias
 alias cdd 'cd ~/Desktop'
 
-# peco
+# peco/z
+function peco_z
+  set -l query (commandline)
+
+  if test -n $query
+    set peco_flags --query "$query"
+  end
+
+  z -l | peco $peco_flags | awk '{ print $2 }' | read recent
+  if [ $recent ]
+      cd $recent
+      commandline -r ''
+      commandline -f repaint
+  end
+end
+
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
+  bind \cq peco_z
 end
 
 # The next line updates PATH for the Google Cloud SDK.
