3e87736a9e0d3ce985b302350a3a1fb51b5c5f5f
takeokunn <bararararatty@gmail.com>
2019-05-28 18:41:47 +0900

    Update fish shell

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -1,5 +1,6 @@
 # load local.fish
 source ~/.config/fish/local.fish
+source ~/.config/fish/functions.fish
 
 # initialize
 function fish_prompt
@@ -11,44 +12,5 @@ function fish_prompt
     printf '%s %s' (set_color yellow)(prompt_pwd) $status_face
 end
 
-function logo
-    echo '                 '(set_color F00)'___
-  ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
-/T            \_'(set_color FF0)'--='(set_color FF7F00)'=='(set_color F00)')
-[ \ '(set_color FF7F00)'('(set_color FF0)'0'(set_color FF7F00)')   '(set_color F00)'\~    \_'(set_color FF0)'-='(set_color FF7F00)'='(set_color F00)')
- \      / )J'(set_color FF7F00)'~~    \\'(set_color FF0)'-='(set_color F00)')
-  \\\\___/  )JJ'(set_color FF7F00)'~'(set_color FF0)'~~   '(set_color F00)'\)
-   \_____/JJJ'(set_color FF7F00)'~~'(set_color FF0)'~~    '(set_color F00)'\\
-   '(set_color FF7F00)'/ '(set_color FF0)'\  '(set_color FF0)', \\'(set_color F00)'J'(set_color FF7F00)'~~~'(set_color FF0)'~~     '(set_color FF7F00)'\\
-  (-'(set_color FF0)'\)'(set_color F00)'\='(set_color FF7F00)'|'(set_color FF0)'\\\\\\'(set_color FF7F00)'~~'(set_color FF0)'~~       '(set_color FF7F00)'L_'(set_color FF0)'_
-  '(set_color FF7F00)'('(set_color F00)'\\'(set_color FF7F00)'\\)  ('(set_color FF0)'\\'(set_color FF7F00)'\\\)'(set_color F00)'_           '(set_color FF0)'\=='(set_color FF7F00)'__
-   '(set_color F00)'\V    '(set_color FF7F00)'\\\\'(set_color F00)'\) =='(set_color FF7F00)'=_____   '(set_color FF0)'\\\\\\\\'(set_color FF7F00)'\\\\
-          '(set_color F00)'\V)     \_) '(set_color FF7F00)'\\\\'(set_color FF0)'\\\\JJ\\'(set_color FF7F00)'J\)
-                      '(set_color F00)'/'(set_color FF7F00)'J'(set_color FF0)'\\'(set_color FF7F00)'J'(set_color F00)'T\\'(set_color FF7F00)'JJJ'(set_color F00)'J)
-                      (J'(set_color FF7F00)'JJ'(set_color F00)'| \UUU)
-                       (UU)'(set_color normal)
-end
-
 # alias
 alias cdd 'cd ~/Desktop'
-
-# peco/z
-function peco_z
-  set -l query (commandline)
-
-  if test -n $query
-    set peco_flags --query "$query"
-  end
-
-  z -l | peco $peco_flags --layout=bottom-up | awk '{ print $2 }' | read recent
-  if [ $recent ]
-      cd $recent
-      commandline -r ''
-      commandline -f repaint
-  end
-end
-
-function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
-  bind \cq peco_z
-end
