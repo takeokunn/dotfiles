59bb48ddcf4aec8867ae99f6fa5f58decab0b877
takeokunn <bararararatty@gmail.com>
2021-08-22 19:38:47 +0900

    Refactor codes

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -1,7 +1,3 @@
-# load local.fish
-source ~/.config/fish/local.fish
-source ~/.config/fish/functions.fish
-
 # initialize
 function fish_prompt
     if [ $status -eq 0 ]
@@ -13,4 +9,16 @@ function fish_prompt
 end
 
 # alias
+alias make 'make -j'
 alias cdd 'cd ~/Desktop'
+
+# keybind
+function fish_user_key_bindings
+    bind \cr peco_select_history
+    bind \co peco_ghq
+    bind \cq peco_z
+end
+
+if type -q tmux && test -z $TMUX && status --is-login
+    tmux_attach_session_if_needed
+end
--- modules/fisher/completions/aws.fish
+++
@@ -1,7 +0,0 @@
-complete -c aws -f -a '(
-    begin
-        set -lx COMP_SHELL fish
-        set -lx COMP_LINE (commandline)
-        aws_completer
-    end
-)'
