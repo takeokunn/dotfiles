f9059e57c1e55fcf226e021a2f5c07ced91ea8b2
takeokunn <bararararatty@gmail.com>
2021-11-08 16:56:51 +0900

    Fix

--- modules/fish/conf.d/php.fish
+++ modules/fish/conf.d/php.fish
@@ -1,8 +1,3 @@
-if type -q php
-    fish_add_path /usr/local/opt/php@8.1/bin
-    fish_add_path /usr/local/opt/php@8.1/sbin
-end
-
 if type -q composer
     set PATH $PATH $HOME/.composer/vendor/bin
     set -x COMPOSER_MEMORY_LIMIT 4G
