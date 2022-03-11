d3281015a9ab6409602b4cb6bd55dc2c7c041563
takeokunn <bararararatty@gmail.com>
2022-03-11 12:43:18 +0900

    Updated skylark command

--- modules/fish/completions/skylark.fish
+++ modules/fish/completions/skylark.fish
@@ -1,5 +1,10 @@
 # subcommands
 complete -f -c skylark -n '__fish_use_subcommand' -xa gusto -d "fetch gusto coupon"
-complete -f -c skylark -n '__fish_use_subcommand' -xa jonathan -d "fetch gusto coupon"
-complete -f -c skylark -n '__fish_use_subcommand' -xa bamiyan -d "fetch gusto coupon"
-
+complete -f -c skylark -n '__fish_use_subcommand' -xa jonathan -d "fetch jonathan coupon"
+complete -f -c skylark -n '__fish_use_subcommand' -xa bamiyan -d "fetch bamiyan coupon"
+complete -f -c skylark -n '__fish_use_subcommand' -xa yumean -d "fetch yumean coupon"
+complete -f -c skylark -n '__fish_use_subcommand' -xa steak_gusto -d "fetch steak_gusto coupon"
+complete -f -c skylark -n '__fish_use_subcommand' -xa steak_gusto -d "fetch grazie_gardens coupon"
+complete -f -c skylark -n '__fish_use_subcommand' -xa aiya -d "fetch aiya coupon"
+complete -f -c skylark -n '__fish_use_subcommand' -xa karayoshi -d "fetch karayoshi coupon"
+complete -f -c skylark -n '__fish_use_subcommand' -xa tonkara_tei -d "fetch tonkara_tei coupon"
--- modules/fish/functions/skylark.fish
+++ modules/fish/functions/skylark.fish
@@ -1,18 +1,30 @@
 function skylark
     set -l brand_id
     switch $argv
-    case "gusto"
-        set brand_id 1
-    case "bamiyan"
-        set brand_id 2
-    case "jonathan"
-        set brand_id 3
-    case '*'
-        echo "invalid argument: please input [gusto, bamiyan, jonathan]"
-        false
+        case "gusto"
+            set brand_id 1
+        case "bamiyan"
+            set brand_id 2
+        case "jonathan"
+            set brand_id 3
+        case "yumean"
+            set brand_id 4
+        case "steak_gusto"
+            set brand_id 5
+        case "grazie_gardens"
+            set brand_id 6
+        case "aiya"
+            set brand_id 7
+        case "karayoshi"
+            set brand_id 13
+        case "tonkara_tei"
+            set brand_id 14
+        case '*'
+            echo "invalid argument: please input [gusto, bamiyan, jonathan yumean steak_gusto grazie_gardens aiya karayoshi tonkara_tei]"
+            return
     end
 
-    curl "https://app-data.skylark-app.net/coupon/normals.json" \
+    curl -s "https://app-data.skylark-app.net/coupon/normals.json" \
         | jq ".[]" \
         | jq "{ brand_id: .brand_id, name: .name, before_price: .before_price, after_price: .after_price, display_number: .display_number }" \
         | jq "select(.brand_id == \"$brand_id\")" \
