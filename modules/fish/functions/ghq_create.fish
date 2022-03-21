c34642c43b97b081f3cb4ca5ea8513602d79eab0
takeokunn <bararararatty@gmail.com>
2022-03-21 22:05:50 +0900

    Fix

--- modules/fish/functions/ghq_create.fish
+++ modules/fish/functions/ghq_create.fish
@@ -1,10 +1,10 @@
 function ghq_create
-    # create github repo
+    # Create github repo
     set license (curl https://api.github.com/licenses | jq -r ".[].spdx_id" | peco)
     gh repo create (echo $argv) --public --license $license -y
     ghq get git@github.com:takeokunn/(echo $argv).git
 
-    # add readme
+    # Add README.md
     cd (ghq root)/github.com/takeokunn/(echo $argv)
     echo "# "(echo $argv) >> README.md
     git add README.md
