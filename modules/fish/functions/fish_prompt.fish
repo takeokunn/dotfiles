05a205871314a9b6f8c9f82f9377acdf512e86c6
takeokunn <bararararatty@gmail.com>
2022-05-11 13:33:34 +0900

    Update

--- modules/fish/functions/fish_prompt.fish
+++ modules/fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
 function fish_prompt
     if [ $status -eq 0 ]
-        set status_face (set_color green)"(*´ω｀*) < "
+        set status_face (set_color 50fa7b)"(*´ω｀*) < "
     else
-        set status_face (set_color red)"｡+ﾟ(∩´﹏'∩)ﾟ+｡ < "
+        set status_face (set_color ff5555)"｡+ﾟ(∩´﹏'∩)ﾟ+｡ < "
     end
-    printf '%s %s' (set_color yellow)(prompt_pwd) $status_face
+    printf '%s %s' (set_color f1fa8c)(prompt_pwd) $status_face
 end
