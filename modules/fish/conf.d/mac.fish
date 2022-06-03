fb3c3bbd130a84a1fe4a5953635b0599550eeeac
takeokunn <bararararatty@gmail.com>
2022-06-03 22:53:25 +0900

    Fix

--- modules/fish/conf.d/mac.fish
+++ modules/fish/conf.d/mac.fish
@@ -10,7 +10,7 @@ if test (uname) = "Darwin"
 
     # libgccjit
     # set -xg CC "/usr/local/bin/gcc-11"
-    # set -xg LIBRARY_PATH (brew --prefix libgccjit)/lib/gcc/9
+    # set -xg LIBRARY_PATH (brew --prefix libgccjit)/lib/gcc/11
 
     # openssl
     fish_add_path /usr/local/opt/openssl/bin
