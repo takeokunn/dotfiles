d92fb3b55ea79e3b6a20531d14a523b05f95b7ea
takeokunn <bararararatty@gmail.com>
2022-08-23 18:42:53 +0900

    Fix

--- modules/nyxt/stylesheet.lisp
+++ modules/nyxt/stylesheet.lisp
@@ -2,28 +2,28 @@
 
 ;; This only works on the versions of Nyxt after 2.2.4.
 (define-configuration browser
-  ((theme (make-instance
-           'theme:theme
-           :dark-p t
-           :background-color "#282a36"
-           :text-color "#f8f8f2"
-           :accent-color "#ff5555"
-           :primary-color "#50fa7b"
-           :secondary-color "#bd93f9"
-           :tertiary-color "#6272a4"
-           :quaternary-color "#44475a"))))
+    ((theme (make-instance
+                'theme:theme
+                :dark-p t
+                :background-color "#282a36"
+                :text-color "#f8f8f2"
+                :accent-color "#ff5555"
+                :primary-color "#50fa7b"
+                :secondary-color "#bd93f9"
+                :tertiary-color "#6272a4"
+                :quaternary-color "#44475a"))))
 
 ;; Custom Dark-mode for webpages
 (define-configuration nyxt/style-mode:dark-mode
-  ((style #.(cl-css:css
-             '((*
-                :background-color "#282a36 !important"
-                :background-image "none !important"
-                :color "#f8f8f2")
-               (a
-                :background-color "#282a36 !important"
-                :background-image "none !important"
-                :color "#6272a4 !important"))))))
+    ((style #.(cl-css:css
+                  '((*
+                        :background-color "#282a36 !important"
+                        :background-image "none !important"
+                        :color "#f8f8f2")
+                       (a
+                           :background-color "#282a36 !important"
+                           :background-image "none !important"
+                           :color "#6272a4 !important"))))))
 
 ;;
 ;; Configurations for versions below 2.2.4:
