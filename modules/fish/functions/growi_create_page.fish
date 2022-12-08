19ffed3db409ef466c7f798442422223d2f04430
takeokunn <bararararatty@gmail.com>
2022-12-08 11:26:49 +0900

    Update

---
+++ modules/fish/functions/growi_create_page.fish
@@ -0,0 +1,14 @@
+function growi_create_page
+    set -l access_token $argv[1]
+    set -l base_url $argv[2]
+    set -l slug (read -p "echo 'slug > '")
+    set -l path "/user/obara/memo/"$slug
+
+    curl -s $base_url"/_api/v3/pages?access_token="$access_token \
+        -X POST \
+        -d "{\"path\": \""$path"\",\"body\": \"# "$slug"\"}" \
+        -H "Content-Type: application/json"
+
+    echo $base_url$path
+    open $base_url$path
+end
