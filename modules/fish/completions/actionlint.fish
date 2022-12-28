18cdfb1a8042c690bff1251bc252ddccdb1c456c
takeokunn <bararararatty@gmail.com>
2022-12-28 16:31:52 +0900

    Update

--- modules/fish/completions/actionlint.fish
+++ modules/fish/completions/actionlint.fish
@@ -6,7 +6,7 @@ complete -c actionlint -o -ignore        -d "Regular expression matching to erro
 complete -c actionlint -o init-config    -d "Generate default config file at .github/actionlint.yaml in current project"
 complete -c actionlint -o no-color       -d "Disable colorful output"
 complete -c actionlint -o oneline        -d "Use one line per one error. Useful for reading error messages from programs"
-complete -c actionlint -o pyflakes       -d "Command name or file path of "pyflakes" external command. If empty, pyflakes integration will be disabled (default "pyflakes")"
+complete -c actionlint -o pyflakes       -d "Command name or file path of "pyflakes" external command. If empty, pyflakes integration will be disabled (default \"pyflakes\")"
 complete -c actionlint -o shellcheck     -d "Command name or file path of \"shellcheck\" external command. If empty, shellcheck integration will be disabled (default \"shellcheck\")"
 complete -c actionlint -o stdin-filename -d "File name when reading input from stdin"
 complete -c actionlint -o verbose        -d "Enable verbose output"
