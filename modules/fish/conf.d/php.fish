723e46c223317ef7110fe8de05337695fc371897
takeokunn <bararararatty@gmail.com>
2023-07-09 17:55:40 +0900

    Update

--- modules/fish/conf.d/php.fish
+++ modules/fish/conf.d/php.fish
@@ -6,3 +6,7 @@ if type -q composer
     fish_add_path $HOME/.composer/vendor/bin
     set -x COMPOSER_MEMORY_LIMIT 4G
 end
+
+if test -d ~/.asdf/installs/php/8.2.3/.composer
+    fish_add_path ~/.asdf/installs/php/8.2.3/.composer/vendor/bin
+end
