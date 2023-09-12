08fd324569e83059fc4432218d3620f50c4e5b19
takeokunn <bararararatty@gmail.com>
2023-09-12 10:41:32 +0900

    Fix

--- modules/fish/conf.d/shell.fish
+++ modules/fish/conf.d/shell.fish
@@ -2,17 +2,13 @@ if type -q fzf and type -q rg
     set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 end
 
-if type -q exa
-    alias ls 'exa'
+if type -q eza
+    alias ls 'eza'
 end
 
 if type -q bat
     set -x MANPAGER 'col -bx | bat -l man -p'
     alias cat 'bat'
-end
-
-if type -q dog
-    alias dig 'dog'
 end
 
 if type -q gping
@@ -21,10 +17,6 @@ end
 
 if type -q dust
     alias du 'dust'
-end
-
-if type -q broot
-    alias tree 'broot -ips'
 end
 
 if type -q pass
