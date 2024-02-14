05eeff1aed121b0db96ec7a3e8854814dcef5fbc
takeokunn <bararararatty@gmail.com>
2024-02-15 00:52:29 +0900

    Add mitamae completion

---
+++ modules/fish/completions/mitamae.fish
@@ -0,0 +1,4 @@
+complete -c mitamae -xa local -d "Run mitamae locally"
+complete -c mitamae -xa help -d "Describe available commands or one specific command"
+complete -c mitamae -xa version -d "Print version"
+complete -c mitamae -s l -l log-level -d "Default: info"
