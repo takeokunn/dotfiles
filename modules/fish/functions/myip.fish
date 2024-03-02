510d462e251cba1f8601bf265253c967f683bd9c
takeokunn <bararararatty@gmail.com>
2024-03-03 00:11:18 +0900

    update

--- modules/fish/functions/myip.fish
+++ modules/fish/functions/myip.fish
@@ -1,5 +1,5 @@
 function myip
-    set -l ip_address (curl -s ifconfig.me)"/32"
+    set -l ip_address (curl -s https://checkip.amazonaws.com)"/32"
     echo $ip_address | pbcopy
     echo $ip_address
 end
