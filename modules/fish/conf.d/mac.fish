75ef185f1a4151fe46ffa0afa2fbac5b6c198c30
takeokunn <bararararatty@gmail.com>
2024-03-05 14:48:54 +0900

    update

--- modules/fish/conf.d/mac.fish
+++ modules/fish/conf.d/mac.fish
@@ -1,3 +1,31 @@
+if test (uname) = "Darwin"
+    # libgccjit
+    set -gx NATIVE_FULL_AOT 1
+
+    # editor
+    set -gx EDITOR "emacs -nw"
+    set -gx HOMEBREW_EDITOR "emacs -nw"
+
+    # path
+    fish_add_path /usr/local/sbin
+    fish_add_path /usr/local/bin
+    fish_add_path $HOME/.local/bin
+
+    fish_add_path $HOME/.local/bin
+
+    # alias
+    alias cdd 'cd ~/Desktop'
+    alias make 'make -j8'
+
+    # emacs
+    alias emacs 'emacs -nw'
+    set -gx LSP_USE_PLISTS true
+
+    # compiler
+    set -gx C clang
+    set -gx CXX clang++
+end
+
 if test -d brew
     # libxml2
     set -gx LDFLAGS "-L$(brew --prefix libxml2)/lib"
@@ -26,30 +54,4 @@ if test -d brew
 
     # gpg
     fish_add_path (brew --prefix gnupg@2.2)/bin
-end
-
-if test (uname) = "Darwin"
-    # libgccjit
-    set -gx NATIVE_FULL_AOT 1
-
-    # editor
-    set -gx EDITOR "emacs -nw"
-    set -gx HOMEBREW_EDITOR "emacs -nw"
-
-    # path
-    fish_add_path /usr/local/sbin
-    fish_add_path /usr/local/bin
-    fish_add_path $HOME/.local/bin
-
-    # alias
-    alias cdd 'cd ~/Desktop'
-    alias make 'make -j8'
-
-    # emacs
-    alias emacs 'emacs -nw'
-    set -gx LSP_USE_PLISTS true
-
-    # compiler
-    set -gx C clang
-    set -gx CXX clang++
 end
