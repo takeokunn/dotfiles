a6fc2317346b8fe781b3c2b4b59f573bd0c30afd
takeokunn <bararararatty@gmail.com>
2024-05-18 17:47:34 +0900

    update

--- modules/fish/conf.d/mac.fish
+++ modules/fish/conf.d/mac.fish
@@ -1,6 +1,7 @@
 if test (uname) = "Darwin"
-    # libgccjit
+    # for emacs
     set -gx NATIVE_FULL_AOT 1
+    set -gx LSP_USE_PLISTS true
 
     # editor
     set -gx EDITOR "emacs -nw"
@@ -11,47 +12,35 @@ if test (uname) = "Darwin"
     fish_add_path /usr/local/bin
     fish_add_path $HOME/.local/bin
 
-    fish_add_path $HOME/.local/bin
-
     # alias
     alias cdd 'cd ~/Desktop'
     alias make 'make -j8'
-
-    # emacs
     alias emacs 'emacs -nw'
-    set -gx LSP_USE_PLISTS true
-
-    # compiler
-    set -gx C clang
-    set -gx CXX clang++
 end
 
 if test -d brew
-    # libxml2
-    set -gx LDFLAGS "-L$(brew --prefix libxml2)/lib"
-    set -gx CPPFLAGS "-I$(brew --prefix libxml2)/include -I$(brew --prefix zlib)/include"
-    set -gx PKG_CONFIG_PATH "$(brew --prefix libxml2)/lib/pkgconfig"
-    fish_add_path (brew --prefix libxml2)/bin
+    # # libxml2
+    # set -gx LDFLAGS "-L$(brew --prefix libxml2)/lib"
+    # set -gx CPPFLAGS "-I$(brew --prefix libxml2)/include -I$(brew --prefix zlib)/include"
+    # set -gx PKG_CONFIG_PATH "$(brew --prefix libxml2)/lib/pkgconfig"
+    # fish_add_path (brew --prefix libxml2)/bin
 
-    # libgccjit
-    set -xg LIBRARY_PATH (brew --prefix libgccjit)/lib/gcc/current
+    # # libgccjit
+    # set -xg LIBRARY_PATH (brew --prefix libgccjit)/lib/gcc/current
 
-    # openssl
-    fish_add_path (brew --prefix openssl)/bin
+    # # openssl
+    # fish_add_path (brew --prefix openssl)/bin
 
-    # texinfo
-    fish_add_path (brew --prefix texinfo)/bin
+    # # texinfo
+    # fish_add_path (brew --prefix texinfo)/bin
 
-    # bison
-    fish_add_path (brew --prefix bison)/bin
+    # # bison
+    # fish_add_path (brew --prefix bison)/bin
 
-    # libiconv
-    fish_add_path (brew --prefix libiconv)/bin
+    # # libiconv
+    # fish_add_path (brew --prefix libiconv)/bin
 
     # homebrew
     fish_add_path /opt/homebrew/bin
     fish_add_path /opt/homebrew/sbin
-
-    # gpg
-    fish_add_path (brew --prefix gnupg@2.2)/bin
 end
