dbe11335310baed6792345281419c429c66f8176
takeokunn <bararararatty@gmail.com>
2024-07-10 10:29:44 +0900

    Fix

--- modules/fish/conf.d/nix.fish
+++ modules/fish/conf.d/nix.fish
@@ -1,17 +1,3 @@
-if test -d /nix/var/nix/profiles/default/bin
-    fish_add_path /nix/var/nix/profiles/default/bin
-end
-
-if test -d /etc/profiles/per-user/obara/bin
-    fish_add_path /etc/profiles/per-user/obara/bin
-end
-
-if test -d ~/.nix-profile/bin
-    fish_add_path ~/.nix-profile/bin
-end
-
-if type -q nix
-    set -gx NIXPKGS_ALLOW_UNFREE 1
-    set -gx NIXPKGS_ALLOW_INSECURE 1
-    set -gx NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM 1
+if test -d /etc/profiles/per-user/(whoami)/bin
+    fish_add_path /etc/profiles/per-user/(whoami)/bin
 end
