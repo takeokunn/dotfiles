07d77375082cc2451bf0d57431a544b404f4c9ec
takeokunn <bararararatty@gmail.com>
2024-07-19 11:26:06 +0900

    Update

--- modules/fish/conf.d/mac.fish
+++ modules/fish/conf.d/mac.fish
@@ -10,37 +10,12 @@ if test (uname) = "Darwin"
     # path
     fish_add_path /usr/local/sbin
     fish_add_path /usr/local/bin
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /opt/homebrew/sbin
     fish_add_path $HOME/.local/bin
 
     # alias
     alias cdd 'cd ~/Desktop'
     alias make 'make -j8'
     alias emacs 'emacs -nw'
-end
-
-if test -d brew
-    # # libxml2
-    # set -gx LDFLAGS "-L$(brew --prefix libxml2)/lib"
-    # set -gx CPPFLAGS "-I$(brew --prefix libxml2)/include -I$(brew --prefix zlib)/include"
-    # set -gx PKG_CONFIG_PATH "$(brew --prefix libxml2)/lib/pkgconfig"
-    # fish_add_path (brew --prefix libxml2)/bin
-
-    # # libgccjit
-    # set -xg LIBRARY_PATH (brew --prefix libgccjit)/lib/gcc/current
-
-    # # openssl
-    # fish_add_path (brew --prefix openssl)/bin
-
-    # # texinfo
-    # fish_add_path (brew --prefix texinfo)/bin
-
-    # # bison
-    # fish_add_path (brew --prefix bison)/bin
-
-    # # libiconv
-    # fish_add_path (brew --prefix libiconv)/bin
-
-    # homebrew
-    fish_add_path /opt/homebrew/bin
-    fish_add_path /opt/homebrew/sbin
 end
